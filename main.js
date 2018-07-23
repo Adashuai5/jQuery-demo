5d187c14416c3ba2aea8cc569bfadaf4153e13c7
adashuai5 <ad873606339@gmail.com>
2018-07-23 23:09:11 +0800

    封装 getSiblings 这个函数

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-var allChildren = item3.parentNode.children
-var array = {
-    length: 0
-}
-for (let i = 0; i < allChildren.length; i++) {
-    if (allChildren[i] !== item3) {
-        array[array.length] = allChildren[i]
-        array.length += 1
+function getSiblings(node) {
+    var allChildren = node.parentNode.children
+    var array = {
+        length: 0
     }
+    for (let i = 0; i < allChildren.length; i++) {
+        if (allChildren[i] !== node) {
+            array[array.length] = allChildren[i]
+            array.length += 1
+        }
+    }
+    return array
 }
-console.log(array)
