9b370ff930e41cd7bfbade9914d0826a298e21cd
adashuai5 <ad873606339@gmail.com>
2018-07-24 21:06:15 +0800

    用声明新的接口的方法取代原来直接在Node上添加属性的方法

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
-Node.prototype.getSiblings = function () {
-    var allChildren = this.parentNode.children
-    var array = {
-        length: 0
-    }
-    for (let i = 0; i < allChildren.length; i++) {
-        if (allChildren[i] !== this) {
-            array[array.length] = allChildren[i]
-            array.length += 1
+window.Node2 = function (node) {
+    return {
+        getSiblings: function () {
+            var allChildren = node.parentNode.children
+            var array = {
+                length: 0
+            }
+            for (let i = 0; i < allChildren.length; i++) {
+                if (allChildren[i] !== node) {
+                    array[array.length] = allChildren[i]
+                    array.length += 1
+                }
+            }
+            return array
+        },
+        addClass: function (classes) {
+            for (let key in classes) {
+                var value = classes[key]
+                var methodName = value ? 'add' : 'remove'
+                node.classList[methodName](key)
+            }
         }
     }
-    return array
 }
 
-Node.prototype.addClass = function (classes) {
-    for (let key in classes) {
-        var value = classes[key]
-        var methodName = value ? 'add' : 'remove'
-        this.classList[methodName](key)
-    }
-}
-
-item3.getSiblings.call(item3)
-item3.addClass.call(item3,{'a':false,'b':true,'c':true})
+var node2 = Node2(item3)
+node2.getSiblings()
+node2.addClass({
+    'a': false,
+    'b': true,
+    'c': true
+})
