a7d944f60a10703fad160910f75c89d3eecc8a75
adashuai5 <ad873606339@gmail.com>
2018-07-26 00:39:18 +0800

    做一个简单的轮播demo

---
+++ jQuery-lunbo-demo/main.js
@@ -0,0 +1,25 @@
+$(p1).on('click', function (x) {
+    $(images).css({
+        transform: 'translateX(0)'
+    })
+})
+$(p2).on('click', function (xx) {
+    $(images).css({
+        transform: 'translateX(-500px)'
+    })
+})
+$(p3).on('click', function (xxx) {
+    $(images).css({
+        transform: 'translateX(-1000px)'
+    })
+})
+$(p4).on('click', function (xxxx) {
+    $(images).css({
+        transform: 'translateX(-1500px)'
+    })
+})
+$(p5).on('click', function (xxxxx) {
+    $(images).css({
+        transform: 'translateX(-2000px)'
+    })
+})
