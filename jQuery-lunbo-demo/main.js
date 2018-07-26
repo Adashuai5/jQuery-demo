bd8d279cde67784412a4b6b7cef01fa901abfa75
adashuai5 <ad873606339@gmail.com>
2018-07-26 23:15:49 +0800

    优化JS代码

--- jQuery-lunbo-demo/main.js
+++ jQuery-lunbo-demo/main.js
@@ -3,36 +3,39 @@ var allButtons = $('#buttons > span')
 for (let i = 0; i < allButtons.length; i++) {
     $(allButtons[i]).on('click', function (x) {
         var index = $(x.currentTarget).index()
-        var n = index * -500
+        var p = index * -500
         $('#images').css({
-            transform: 'translate(' + n + 'px)'
+            transform: 'translate(' + p + 'px)'
         })
-        allButtons.eq(n)
-            .addClass('red')
-            .siblings('.red').removeClass('red')
+        n = index
+        activeButton(allButtons.eq(n))
     })
 }
 
 var n = 0
 var size = allButtons.length
-allButtons.eq(n % size).trigger('click')
-    .addClass('red')
-    .siblings('.red').removeClass('red')
-var timerID = setInterval(() => {
-    n += 1
-    allButtons.eq(n % size).trigger('click')
+playSilde(n % size)
+var timerID = setTimer()
+
+function setTimer() {
+    return setInterval(() => {
+        n += 1
+        playSilde(n % size)
+    }, 1000)
+}
+
+function playSilde(index) {
+    allButtons.eq(index).trigger('click')
+}
+
+function activeButton($button) {
+    $button
         .addClass('red')
         .siblings('.red').removeClass('red')
-}, 1000)
-
+}
 $('.window').on('mouseenter', function () {
     window.clearInterval(timerID)
 })
 $('.window').on('mouseleave', function () {
-    window.setInterval(() => {
-        n += 1
-        allButtons.eq(n % size).trigger('click')
-            .addClass('red')
-            .siblings('.red').removeClass('red')
-    }, 1000)
+    timerID = setTimer()
 })
