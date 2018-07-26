6ceba37572c96c00ce497af7f6cff7c48f48946b
adashuai5 <ad873606339@gmail.com>
2018-07-26 21:23:00 +0800

    优化JS代码，变button为span

--- jQuery-lunbo-demo/main.js
+++ jQuery-lunbo-demo/main.js
@@ -1,25 +1,11 @@
-$(p1).on('click', function (x) {
-    $(images).css({
-        transform: 'translateX(0)'
+var allButtons = $('#buttons > span')
+
+for (let i = 0; i < allButtons.length; i++) {
+    $(allButtons[i]).on('click', function (x) {
+        var index = $(x.currentTarget).index()
+        var n = index * -500
+        $('#images').css({
+            transform: 'translate(' + n + 'px)'
+        })
     })
-})
-$(p2).on('click', function (xx) {
-    $(images).css({
-        transform: 'translateX(-500px)'
-    })
-})
-$(p3).on('click', function (xxx) {
-    $(images).css({
-        transform: 'translateX(-1000px)'
-    })
-})
-$(p4).on('click', function (xxxx) {
-    $(images).css({
-        transform: 'translateX(-1500px)'
-    })
-})
-$(p5).on('click', function (xxxxx) {
-    $(images).css({
-        transform: 'translateX(-2000px)'
-    })
-})
+}
