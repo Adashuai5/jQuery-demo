cf5d201112cec0b9f10900f6a16ff1e62986c35a
adashuai5 <ad873606339@gmail.com>
2018-07-27 23:54:21 +0800

    初步完成

--- jQuery-lian/main.js
+++ jQuery-lian/main.js
@@ -7,12 +7,32 @@ for (let i = 0; i < allButtons.length; i++) {
         $('#images').css({
             transform: 'translateX(' + px + 'px)'
         })
+        n = index
+        allButtons.eq(n)
+            .addClass('red')
+            .siblings('.red').removeClass('red')
     })
 }
 
 var n = 0
-allButtons.eq(n % 5).trigger('click')
-setInterval(() => {
+var size = allButtons.length
+allButtons.eq(n % size).trigger('click')
+    .addClass('red')
+    .siblings('.red').removeClass('red')
+var timerID = setInterval(() => {
     n += 1
-    allButtons.eq(n % 5).trigger('click')
+    allButtons.eq(n % size).trigger('click')
+        .addClass('red')
+        .siblings('.red').removeClass('red')
 }, 1000)
+$('.window').on('mouseenter', function () {
+    window.clearInterval(timerID)
+})
+$('.window').on('mouseleave', function () {
+    window.setInterval(() => {
+        n += 1
+        allButtons.eq(n % size).trigger('click')
+            .addClass('red')
+            .siblings('.red').removeClass('red')
+    }, 1000)
+})
