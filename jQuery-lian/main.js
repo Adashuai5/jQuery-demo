bd85d3d5c4957d14e9513a13f48558329a2715a8
adashuai5 <ad873606339@gmail.com>
2018-07-27 22:59:09 +0800

    添加button，实现轮播

---
+++ jQuery-lian/main.js
@@ -0,0 +1,25 @@
+$(p1).on('click', function () {
+    $(images).css({
+        transform: 'translateX(0)'
+    })
+})
+$(p2).on('click', function () {
+    $(images).css({
+        transform: 'translateX(-500px)'
+    })
+})
+$(p3).on('click', function () {
+    $(images).css({
+        transform: 'translateX(-1000px)'
+    })
+})
+$(p4).on('click', function () {
+    $(images).css({
+        transform: 'translateX(-1500px)'
+    })
+})
+$(p5).on('click', function () {
+    $(images).css({
+        transform: 'translateX(-2000px)'
+    })
+})
