cd305677e6159adc797c9ab0072778aafdab302f
adashuai5 <ad873606339@gmail.com>
2018-07-28 14:24:06 +0800

    写一个轮播

---
+++ jQuery-lian/main.js
@@ -0,0 +1,38 @@
+var allButtons = $('#buttons > span')
+
+for (let i = 0; i < allButtons.length; i++) {
+    $(allButtons[i]).on('click', function (x) {
+        var index = $(x.currentTarget).index()
+        var px = index * -500
+        $('#images').css({
+            transform: 'translate(' + px + 'px)'
+        })
+        n = index
+        allButtons.eq(n)
+            .addClass('red')
+            .siblings('.red').removeClass('red')
+    })
+}
+
+var n = 0
+var size = allButtons.length
+playSilde(n % size)
+var timerId = setTimer()
+
+$('.window').mouseenter(function () {
+    window.clearInterval(timerId)
+})
+$('.window').mouseleave(function () {
+    timerId = setTimer()
+})
+
+function playSilde(index){
+    allButtons.eq(index).trigger('click')
+}
+
+function setTimer(){
+    return setInterval(() => {
+        n += 1
+        playSilde(n % size)
+    }, 1000)
+}
