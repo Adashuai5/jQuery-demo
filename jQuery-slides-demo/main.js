1f74c7dcfd1c436e57cfea3f31299fb83f68731c
adashuai5 <ad873606339@gmail.com>
2018-07-29 18:08:30 +0800

    优化 jQuery 代码

--- jQuery-slides-demo/main.js
+++ jQuery-slides-demo/main.js
@@ -1,18 +1,21 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-$('.images > img:nth-child(4)').addClass('enter')
-$('.images > img:nth-child(5)').addClass('enter')
-
-let n = 1
+let n
+初始化()
 setInterval(() => {
-    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+    makeLeave(getImage(n))
         .one('transitionend', (e) => {
-            $(e.currentTarget).removeClass('leave').addClass('enter')
+            makeEnter($(e.currentTarget))
         })
-    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+    makeCurrent(getImage(n + 1))
     n += 1
 }, 3000)
+
+
+
+
+
+function getImage(n) {
+    return $(`.images > img:nth-child(${x(n)})`)
+}
 
 function x(n) {
     if (n > 5) {
@@ -22,4 +25,22 @@ function x(n) {
         }
     }
     return n
+}
+
+function 初始化() {
+    n = 1
+    $(getImage(n)).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function makeCurrent($node) {
+    return $node.removeClass('leave enter').addClass('current')
+}
+
+function makeLeave($node) {
+    return $node.removeClass('current enter').addClass('leave')
+}
+
+function makeEnter($node) {
+    return $node.removeClass('leave current').addClass('enter')
 }
