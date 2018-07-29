c9393acc81b2382f6ba9855ffc9dff02b33116cf
adashuai5 <ad873606339@gmail.com>
2018-07-29 17:31:14 +0800

    用jQuery写这个slides

--- jQuery-slides-demo/main.js
+++ jQuery-slides-demo/main.js
@@ -1,78 +1,25 @@
-setTimeout(function () {
-    $('.images>img:nth-child(1)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images>img:nth-child(2)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images>img:nth-child(1)').one('transitionend', function (e) {
-        $(e.currentTarget).addClass('right').css({
-            transform: 'none'
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(2)').addClass('enter')
+$('.images > img:nth-child(3)').addClass('enter')
+$('.images > img:nth-child(4)').addClass('enter')
+$('.images > img:nth-child(5)').addClass('enter')
+
+let n = 1
+setInterval(() => {
+    $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+        .one('transitionend', (e) => {
+            $(e.currentTarget).removeClass('leave').addClass('enter')
         })
-    })
-}, 2000)
-setTimeout(function () {
-    $('.images>img:nth-child(2)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images>img:nth-child(3)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images>img:nth-child(2)').one('transitionend', function (e) {
-        $(e.currentTarget).addClass('right').css({
-            transform: 'none'
-        })
-    })
-}, 4000)
-setTimeout(function () {
-    $('.images>img:nth-child(3)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images>img:nth-child(4)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images>img:nth-child(3)').one('transitionend', function (e) {
-        $(e.currentTarget).addClass('right').css({
-            transform: 'none'
-        })
-    })
-}, 6000)
-setTimeout(function () {
-    $('.images>img:nth-child(4)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images>img:nth-child(5)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images>img:nth-child(4)').one('transitionend', function (e) {
-        $(e.currentTarget).addClass('right').css({
-            transform: 'none'
-        })
-    })
-}, 8000)
-setTimeout(function () {
-    $('.images>img:nth-child(5)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images>img:nth-child(1)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images>img:nth-child(5)').one('transitionend', function (e) {
-        $(e.currentTarget).addClass('right').css({
-            transform: 'none'
-        })
-    })
-}, 10000)
-setTimeout(function () {
-    $('.images>img:nth-child(1)').css({
-        transform: 'translateX(-200%)'
-    })
-    $('.images>img:nth-child(2)').css({
-        transform: 'translateX(-100%)'
-    })
-    $('.images>img:nth-child(1)').one('transitionend', function (e) {
-        $(e.currentTarget).addClass('right').css({
-            transform: 'none'
-        })
-    })
-}, 12000)
+    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+    n += 1
+}, 3000)
+
+function x(n) {
+    if (n > 5) {
+        n = n % 5
+        if (n === 0) {
+            n = 5
+        }
+    }
+    return n
+}
