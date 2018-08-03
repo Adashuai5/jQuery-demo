0159f5e8ab92310eca4fc5abf6894c26a055f2b4
adashuai5 <ad873606339@gmail.com>
2018-08-04 01:33:19 +0800

    点击关闭浮层和理解DOM事件

--- DOM-demo/main.js
+++ DOM-demo/main.js
@@ -7,3 +7,11 @@ $(clickMe).on('click', function () {
 $(wrapper).on('click', function (e) {
     e.stopPropagation()
 })
+
+var n = 0
+$('div').on('click', function (e) {
+    setTimeout(function () {
+        $(e.currentTarget).addClass('active')
+    }, n * 1000)
+    n += 1
+})
