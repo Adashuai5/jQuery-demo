f07de1a32934f481989237e07e0b16f54f3f7481
adashuai5 <ad873606339@gmail.com>
2018-08-05 01:36:34 +0800

    写一个苹果公司风格的无缝轮播

---
+++ 苹果风格轮播/main.js
@@ -0,0 +1,92 @@
+let $buttons = $('#buttonWrapper > button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
+makeFakeSlides()
+$slides.css({
+    transform: 'translateX(-920px)'
+})
+bindEvents()
+$(next).on('click', function () {
+    goToSlide(current - 1)
+})
+$(previous).on('click', function () {
+    goToSlide(current + 1)
+})
+
+setTimer()
+
+$('.container').on('mouseenter', function () {
+    window.clearInterval(timer)
+}).on('mouseleave', function () {
+    setTimer()
+})
+
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        window.clearInterval(timer)
+    } else {
+        setTimer()
+    }
+})
+
+function setTimer() {
+    timer = setInterval(function () {
+        goToSlide(current + 1)
+    }, 2000)
+}
+
+function bindEvents() {
+
+    $('#buttonWrapper').on('click', 'button', function (e) {
+        let $button = $(e.currentTarget)
+        let index = $button.index()
+        goToSlide(index)
+    })
+}
+
+function goToSlide(index) {
+    if (index > $buttons.length - 1) {
+        index = 0
+    } else if (index < 0) {
+        index = $buttons.length - 1
+    }
+    if (current === $buttons.length - 1 && index === 0) {
+        $slides.css({
+                transform: `translateX(${- ($buttons.length + 1) * 920}px)`
+            })
+            .one('transitionend', function () {
+                $slides.hide()
+                    .offset()
+                $slides.css({
+                        transform: 'translateX(-920px)'
+                    })
+                    .show()
+            })
+    } else if (current === 0 && index === $buttons.length - 1) {
+        $slides.css({
+                transform: 'translateX(0px)'
+            })
+            .one('transitionend', function () {
+                $slides.hide()
+                    .offset()
+                $slides.css({
+                        transform: `translateX(${- (index+1) * 920}px)`
+                    })
+                    .show()
+            })
+    } else {
+        $slides.css({
+            transform: `translateX(${- (index+1) * 920}px)`
+        })
+    }
+    current = index
+}
+
+function makeFakeSlides() {
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length - 1).clone(true)
+
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
+}
