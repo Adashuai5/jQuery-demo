6237e65588a521aca846ee08dbb584ccf24d31c8
adashuai5 <ad873606339@gmail.com>
2018-08-04 14:36:43 +0800

    再次点击按钮关闭浮层

--- DOM-demo/main.js
+++ DOM-demo/main.js
@@ -1,17 +1,22 @@
 $(clickMe).on('click', function () {
-    $(popover).show()
-    $(document).one('click', function () {
+    if (popover.style.display === 'block') {
         $(popover).hide()
-    })
+    } else {
+        $(popover).show()
+        $(document).one('click', function () {
+            $(popover).hide()
+        })
+    }
 })
 $(wrapper).on('click', function (e) {
     e.stopPropagation()
 })
 
+// 理解 DOM 事件
 var n = 0
 $('div').on('click', function (e) {
     setTimeout(function () {
         $(e.currentTarget).addClass('active')
-    }, n * 1000)
+    }, n * 500)
     n += 1
 })
