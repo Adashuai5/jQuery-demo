b4cd2dd85be88987ad5304c4a1dd42abbe96f48c
adashuai5 <ad873606339@gmail.com>
2018-08-11 14:17:53 +0800

    添加div默认背景颜色为白色，添加remove事件

--- DOM-demo/main.js
+++ DOM-demo/main.js
@@ -13,10 +13,23 @@ $(wrapper).on('click', function (e) {
 })
 
 // 理解 DOM 事件
-var n = 0
-$('div').on('click', function (e) {
-    setTimeout(function () {
-        $(e.currentTarget).addClass('active')
+let divs = $('div').get()
+let n = 0
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', () => {
+    setTimeout(() => {
+      divs[i].classList.add('active')
     }, n * 500)
     n += 1
-})
+  }, true)
+}
+
+
+for (let i = 0; i < divs.length; i++) {
+  divs[i].addEventListener('click', () => {
+    setTimeout(() => {
+      divs[i].classList.remove('active')
+    }, n * 500)
+    n += 1
+  })
+}
